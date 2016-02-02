7315db422929058808922c95d1d4b8424104ab3c
ncadiente <ncadiente@gmail.com>
2016-02-01 19:25:46 -1000

    begins authentication

--- server.js
+++ server.js
@@ -1,16 +1,70 @@
 var express = require('express');
 var app = express();
+var CONFIG = require('./config/config.js');
 var bodyParser = require('body-parser');
 var db = require('./models');
 var Photo = db.Photo;
+var Users = db.Users;
+var passport = require('passport');
+var session = require('express-session');
+var LocalStrategy = require('passport-local').Strategy;
 var methodOverride = require('method-override');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(session(CONFIG.SESSION));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', 'templates');
 app.set('view engine', 'jade');
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done){
+    var isAuthenticated = authenticate(username, password);
+    if(!isAuthenticated){
+      return done(null, false);
+    }
+    console.log("in strategy");
+    var user;
+    Users.findOne({where : {
+    username : username
+    }})
+    .then(function(data){
+      user = data;
+      return done(null, user);
+    });
+  }
+));
+
+function authenticate(username, password){
+  console.log("authenticate function");
+  var userAuth;
+  Users.findOne({where : {
+    username : username
+  }})
+    .then(function(data){
+      userAuth = data;
+    })
+    .then(function(data){
+      return(userAuth.username === username && userAuth.password === password);
+    });
+}
+
+function isAuthenticated(req,res,next){
+  if(!req.isAuthenticated()){
+    return res.redirect('/login');
+  }
+  console.log('authenticated');
+  return next();
+}
 
 app.use( methodOverride(function( req, res ) {
   if( req.body && typeof req.body === 'object' && '_method' in req.body ) {
@@ -18,6 +72,15 @@ app.use( methodOverride(function( req, res ) {
     delete req.body._method;
     return method;
   }
+}));
+
+app.get('/login', function(req, res){
+  res.render('photos/login');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect : '/new',
+  failureRedirect : '/login'
 }));
 
 app.get('/', function(req, res) {
